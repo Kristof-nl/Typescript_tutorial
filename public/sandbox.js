a7be6ced363454186456524a258cec2d3696d097
Kristof-nl <hermaszewski.krzysztof@gmail.com>
2022-03-11 13:49:49 +0100

    Changes in the structure

---
+++ public/sandbox.js
@@ -0,0 +1,13 @@
+"use strict";
+let character = 'luigi';
+let age = 30;
+let blackBelt = false;
+character = 'luigi';
+age = 40;
+blackBelt = true;
+const circ = (diameter) => {
+    return diameter * Math.PI;
+};
+console.log("test");
+console.log("test2");
+console.log("test3");
---
+++ src/sandbox.ts
@@ -0,0 +1,17 @@
+let character = 'luigi';
+let age = 30;
+let blackBelt = false;
+
+character = 'luigi';
+
+age = 40;
+
+blackBelt = true;
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+};
+
+console.log("test");
+console.log("test2");
+console.log("test3");
